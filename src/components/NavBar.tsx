424b71206de4a6aa261e255aaf60196f3caba95c
GlebTc <bob88tikhonov@gmail.com>
2022-06-20 09:10:20 -0400

    Active button implemented

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, animateScroll as scroll } from "react-scroll"; //https://www.digitalocean.com/community/tutorials/how-to-implement-smooth-scrolling-in-react
+import { Link, /*animateScroll as scroll*/ } from "react-scroll"; //https://www.digitalocean.com/community/tutorials/how-to-implement-smooth-scrolling-in-react
 
 
 const NavBar: React.FC = () => {
@@ -7,45 +7,49 @@ const NavBar: React.FC = () => {
     return(
         <nav className="nav__container">
             <h1>Gleb Tc</h1>
-            <ul className="nav__btns__container">
+            <ul className="nav__links__container">
                 <Link
                     activeClass="active"
                     to="home"
                     spy={true}
                     smooth={true}
                     duration={500}
+                    offset={-55}
                 >
-                    <li className="nav__btn">Home</li>
+                    <li className="nav__link">Home</li>
                 </Link>
 
                 <Link
+                    activeClass="active"
                     to="about"
                     spy={true}
                     smooth={true}
                     duration={500}
                     offset={-55}
                 >
-                    <li className="nav__btn">About</li>
+                    <li className="nav__link">About</li>
                 </Link>
 
                 <Link
+                    activeClass="active"
                     to="portfolio"
                     spy={true}
                     smooth={true}
                     duration={500}
                     offset={-55}
                 >
-                    <li className="nav__btn">Portfolio</li>
+                    <li className="nav__link">Portfolio</li>
                 </Link>
                 
                 <Link
+                    activeClass="active"
                     to="contact"
                     spy={true}
                     smooth={true}
                     duration={500}
                     offset={-55}
                 >
-                    <li className="nav__btn">Contact</li>
+                    <li className="nav__link">Contact</li>
                 </Link>
             </ul>
         </nav>
