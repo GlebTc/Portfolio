8c72c9996b40ea108d1ed938547792d34333b012
GlebTc <bob88tikhonov@gmail.com>
2022-08-18 00:11:01 -0400

    Carousel implemented for portfolio section

--- src/components/pages__components/Portfolio.tsx
+++ src/components/pages__components/Portfolio.tsx
@@ -1,20 +1,42 @@
 //create a carousel for the portfolio with three items
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { CarouselItem } from 'react-bootstrap';
 
+type portfolioProps = {
+  title: string,
+  id: string
+}
 
-const Portfolio = ({title, id}:{title: string, id:string}) => {
+const Portfolio = ({title, id}: portfolioProps) => {
   return (
     <div className="portfolio">
       <h1>{title}</h1>
-      <div className="portfolio-gallery">
-        <div className="portfolio-gallery-item">
-          <a href="https://candid-donut-35bb17.netlify.app/" target="_blank" rel="noreferrer">
-            <img src="portfolio__item__1.png" alt="candid-donut-35bb17" className="portfolio__image"/>
-          </a>
+      <Carousel className="portfolio__gallery">
+        <Carousel.Item>
+          <div className="portfolio__gallery__item">
+            <h2>Edo Reno</h2>
+            <a href="https://edoreno.netlify.app/" target="_blank" rel="noreferrer">
+              <img src="portfolio__item__1.png" alt="Edo Reno Website" className="portfolio__image"/>
+            </a>
+            <div className="portfolio__gallery__item__description">
+              <p>This is one of the first projects in <span>React JS</span>.  The focus of this project was to create a <span>Mobile-First UI</span> and create a functional contact form.  There is no backend and an <span>EmailJS</span> package was used to add functionality to the contact form.</p>
+            </div>
           </div>
-      </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="portfolio__gallery__item">
+            <h2>Pet Finder</h2>
+            <a href="https://edoreno.netlify.app/" target="_blank" rel="noreferrer">
+              <img src="portfolio__item__1.png" alt="Edo Reno Website" className="portfolio__image"/>
+            </a>
+            <div className="portfolio__gallery__item__description">
+              <p>This is one of the first projects in <span>React JS</span>.  The focus of this project was to create a <span>Mobile-First UI</span> and create a functional contact form.  There is no backend and an <span>EmailJS</span> package was used to add functionality to the contact form.</p>
+            </div>
+          </div>
+        </Carousel.Item>
+      </Carousel>
     </div>
-
   );
 }
     
