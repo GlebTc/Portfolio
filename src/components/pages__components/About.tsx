af774e17cb5f99639931b27a23f1dc295ba08ab2
GlebTc <bob88tikhonov@gmail.com>
2022-08-18 06:28:44 -0400

    Working on scroll bars

--- src/components/pages__components/About.tsx
+++ src/components/pages__components/About.tsx
@@ -7,21 +7,21 @@ const About = ({title, id}:{title:string, id:string}) => {
         <section id={id} className="about__container">
             <h1>{title} Me</h1>
             <p className="about" id={title}>
-                Educational Background:<br />
+                Educational Background:<br /> <br />
                 <ul>
                     <li><span>Frontend Developer Bootcamp</span></li>
                     <li><span>BBA (Finance)</span></li>
                     <li><span>International Marketing Management</span></li>
                     <li><span>Business Marketing</span></li>
                 </ul>
-                Professional Background:<br />
+                Professional Background:<br /> <br />
                 <ul>
                     <li><span>Real Estate</span></li>
                     <li><span>Mortgage Financing</span></li>
                     <li><span>Life Insurance</span></li>
                     <li><span>Healthcare</span></li>
                 </ul>
-                I have experience working individually, as part of a team, and as a member of a large organization. Working individually, I am <span>very organized</span>, <span>detail oriented</span> and committed to <span>high level of efficiency</span>. As part of a team, I am very <span>supportive</span>, embrace constructive criticism and always ready to assist.
+                I have experience working individually, as part of a team, and as a member of a large organization. Working individually, I am very <span>organized</span>, <span>detail oriented</span> and committed to <span>high level of efficiency</span>. As part of a team, I am very <span>supportive</span>, embrace constructive criticism and always ready to assist.
             </p>
         </section>
     )
--- src/components/pages__components/Home.tsx
+++ src/components/pages__components/Home.tsx
@@ -3,9 +3,9 @@ const Home = ({title, id}:{title: string, id:string}) => {
         <section id={id} className="home__container">
             <h1>{title}</h1>
             <p className="introduction" id={title}>
-                Welcome to my website, <br />
+                Welcome to my website, <br /> <br />
 
-                My name is <span>Gleb Tchani</span> and I am a self-taught software developer in the early stages of my career.  I have approximately 2 years of experience working with various frontend technologies.<br /><br />
+                My name is <span>Gleb Tchani</span> and I am a self-taught software developer in the early stages of my career.  I have approximately 2 years of experience working with various frontend technologies.  As yo unavigate to my portfolio, yo ucan see that I have experience with the following stack:<br /><br />
                 <ul>
                     <li><span>HTML 5</span></li>
                     <li><span>CSS 3 (SCSS)</span></li>
@@ -13,8 +13,8 @@ const Home = ({title, id}:{title: string, id:string}) => {
                     <li><span>React JS</span></li>
                 </ul>
                 <br />
-                I have since taken on small individual projects to build websites for local businesses as well as freelance contracting opportunities to improve existing websites.<br /><br />
-                Currently I am working on implementing <span>TypeScript</span> into my code and am hoping to soon begin exploring <span>Node JS</span> to have better understanding of backend.
+                I have since taken on small individual projects to build websites for local businesses, freelance contracting opportunities to improve existing websites as well as projects of my own to improve my knowledge and expertise.<br /><br />
+                Currently I am working on implementing <span>TypeScript</span> into my code and exploring <span>Node JS</span> and <span>Express</span> to have better understanding of backend.
             </p>
         </section>
     )
