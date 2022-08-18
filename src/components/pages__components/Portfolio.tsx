75b9834bfe7bd0af16ede99e7ff2be0af85090f5
GlebTc <bob88tikhonov@gmail.com>
2022-08-18 03:24:38 -0400

    Added second item to the portfolio section.

--- src/components/pages__components/Portfolio.tsx
+++ src/components/pages__components/Portfolio.tsx
@@ -17,21 +17,21 @@ const Portfolio = ({title, id}: portfolioProps) => {
           <div className="portfolio__gallery__item">
             <h2>Edo Reno</h2>
             <a href="https://edoreno.netlify.app/" target="_blank" rel="noreferrer">
-              <img src="portfolio__item__1.png" alt="Edo Reno Website" className="portfolio__image"/>
+              <img src="portfolio__item__1.png" alt="Edo Reno Website" />
             </a>
             <div className="portfolio__gallery__item__description">
-              <p>This is one of the first projects in <span>React JS</span>.  The focus of this project was to create a <span>Mobile-First UI</span> and create a functional contact form.  There is no backend and an <span>EmailJS</span> package was used to add functionality to the contact form.</p>
+              <p>This is one of the first projects in <span>React JS</span>.  The focus of this project was to create a <span>Mobile-First UI</span> using <span>SCSS</span> and create a functional contact form.  There is no backend and an <span>EmailJS</span> package was used to add functionality to the contact form.</p>
             </div>
           </div>
         </Carousel.Item>
         <Carousel.Item>
           <div className="portfolio__gallery__item">
             <h2>Pet Finder</h2>
-            <a href="https://edoreno.netlify.app/" target="_blank" rel="noreferrer">
-              <img src="portfolio__item__1.png" alt="Edo Reno Website" className="portfolio__image"/>
+            <a href="https://petfindertool.netlify.app" target="_blank" rel="noreferrer">
+              <img src="portfolio__item__2.png" alt="Pet Finder Tool" />
             </a>
             <div className="portfolio__gallery__item__description">
-              <p>This is one of the first projects in <span>React JS</span>.  The focus of this project was to create a <span>Mobile-First UI</span> and create a functional contact form.  There is no backend and an <span>EmailJS</span> package was used to add functionality to the contact form.</p>
+              <p>This is my second project where I have added extra features.  I have implemented <span>Custom Reusable Components</span>, implemented the use of <span>axios</span> to fetch data from PetFinder Api.  For the website design I used <span>Material UI</span>.</p>
             </div>
           </div>
         </Carousel.Item>
